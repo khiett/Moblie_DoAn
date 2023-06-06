e024eb0a4a227b6150955c963ba1b951c7bfca89
Trương Minh Khiết <19520628@gm.uit.edu.vn>
2023-06-06 10:44:52 +0700

    finish

--- NoteApp/app/src/main/java/com/example/noteapp/NoteDetailsActivity.java
+++ NoteApp/app/src/main/java/com/example/noteapp/NoteDetailsActivity.java
@@ -1,10 +1,19 @@
 package com.example.noteapp;
 
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,14 +23,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 
+import java.util.Calendar;
+
 public class NoteDetailsActivity extends AppCompatActivity {
 
-    EditText titleEditText,contentEditText;
+    EditText titleEditText, contentEditText;
     ImageButton saveNoteBtn;
     TextView pageTitleTextView;
-    String title,content,docId;
+    String title, content, docId;
     boolean isEditMode = false;
     TextView deleteNoteTextViewBtn;
+    Button setReminderBtn;
+
+    private String noteTitle;
+    private String noteContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,34 +47,104 @@ public class NoteDetailsActivity extends AppCompatActivity {
         contentEditText = findViewById(R.id.notes_content_text);
         saveNoteBtn = findViewById(R.id.save_note_btn);
         pageTitleTextView = findViewById(R.id.page_title);
-        deleteNoteTextViewBtn  = findViewById(R.id.delete_note_text_view_btn);
+        deleteNoteTextViewBtn = findViewById(R.id.delete_note_text_view_btn);
+        setReminderBtn = findViewById(R.id.set_reminder_btn);
 
-        //receive data
+        // Nhận dữ liệu từ Intent
         title = getIntent().getStringExtra("title");
-        content= getIntent().getStringExtra("content");
+        content = getIntent().getStringExtra("content");
         docId = getIntent().getStringExtra("docId");
 
-        if(docId!=null && !docId.isEmpty()){
+        if (docId != null && !docId.isEmpty()) {
             isEditMode = true;
         }
 
         titleEditText.setText(title);
         contentEditText.setText(content);
-        if(isEditMode){
+        if (isEditMode) {
             pageTitleTextView.setText("Edit your note");
             deleteNoteTextViewBtn.setVisibility(View.VISIBLE);
         }
 
-        saveNoteBtn.setOnClickListener( (v)-> saveNote());
+        saveNoteBtn.setOnClickListener((v) -> saveNote());
 
-        deleteNoteTextViewBtn.setOnClickListener((v)-> deleteNoteFromFirebase() );
+        deleteNoteTextViewBtn.setOnClickListener((v) -> deleteNoteFromFirebase());
 
+        setReminderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Xử lý khi người dùng nhấp vào nút "Set Reminder" ở đây
+                showDateTimePicker();
+            }
+        });
     }
 
-    void saveNote(){
-        String noteTitle = titleEditText.getText().toString();
-        String noteContent = contentEditText.getText().toString();
-        if(noteTitle==null || noteTitle.isEmpty() ){
+    private void showDateTimePicker() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        // Hiển thị DatePickerDialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(NoteDetailsActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        // Lưu ngày đã chọn và hiển thị TimePickerDialog
+                        final int selectedYear = year;
+                        final int selectedMonth = monthOfYear;
+                        final int selectedDay = dayOfMonth;
+
+                        // Hiển thị TimePickerDialog
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(NoteDetailsActivity.this,
+                                new TimePickerDialog.OnTimeSetListener() {
+                                    @Override
+                                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                        // Lưu giá trị ngày và giờ đã chọn
+                                        Calendar calendar = Calendar.getInstance();
+                                        calendar.set(selectedYear, selectedMonth, selectedDay, hourOfDay, minute);
+
+                                        // Tạo Intent cho BroadcastReceiver của bạn (MyAlarmReceiver)
+                                        Intent intent = new Intent(NoteDetailsActivity.this, MyAlarmReceiver.class);
+                                        intent.setAction("com.example.noteapp.ALARM_ACTION");
+                                        intent.putExtra("title", NoteDetailsActivity.this.noteTitle);
+                                        intent.putExtra("content", NoteDetailsActivity.this.noteContent);
+
+                                        // Tạo PendingIntent từ Intent
+                                        PendingIntent pendingIntent = PendingIntent.getBroadcast(NoteDetailsActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+                                        // Lấy thời gian trong milliseconds từ calendar
+                                        long alarmTimeMillis = calendar.getTimeInMillis();
+
+                                        // Đặt báo thức sử dụng AlarmManager
+                                        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                                        if (alarmManager != null) {
+                                            // Đặt báo thức theo thời gian đã chọn
+                                            alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTimeMillis, pendingIntent);
+                                        }
+
+                                        // Hiển thị thông báo cho người dùng
+                                        String selectedDateTime = selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear
+                                                + " " + hourOfDay + ":" + minute;
+                                        Utility.showToast(NoteDetailsActivity.this, "Alarm set for: " + selectedDateTime);
+                                    }
+                                },
+                                hour, minute, true);
+
+                        timePickerDialog.show();
+                    }
+                },
+                year, month, day);
+
+        datePickerDialog.show();
+    }
+
+    void saveNote() {
+        noteTitle = titleEditText.getText().toString();
+        noteContent = contentEditText.getText().toString();
+        if (noteTitle == null || noteTitle.isEmpty()) {
             titleEditText.setError("Title is required");
             return;
         }
@@ -69,52 +154,46 @@ public class NoteDetailsActivity extends AppCompatActivity {
         note.setTimestamp(Timestamp.now());
 
         saveNoteToFirebase(note);
-
     }
 
-    void saveNoteToFirebase(Note note){
+    void saveNoteToFirebase(Note note) {
         DocumentReference documentReference;
-        if(isEditMode){
+        if (isEditMode) {
             //update the note
             documentReference = Utility.getCollectionReferenceForNotes().document(docId);
-        }else{
+        } else {
             //create new note
             documentReference = Utility.getCollectionReferenceForNotes().document();
         }
 
-
-
         documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     //note is added
-                    Utility.showToast(NoteDetailsActivity.this,"Note added successfully");
+                    Utility.showToast(NoteDetailsActivity.this, "Note added successfully");
                     finish();
-                }else{
-                    Utility.showToast(NoteDetailsActivity.this,"Failed while adding note");
+                } else {
+                    Utility.showToast(NoteDetailsActivity.this, "Failed while adding note");
                 }
             }
         });
-
     }
 
-    void deleteNoteFromFirebase(){
+    void deleteNoteFromFirebase() {
         DocumentReference documentReference;
         documentReference = Utility.getCollectionReferenceForNotes().document(docId);
         documentReference.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     //note is deleted
-                    Utility.showToast(NoteDetailsActivity.this,"Note deleted successfully");
+                    Utility.showToast(NoteDetailsActivity.this, "Note deleted successfully");
                     finish();
-                }else{
-                    Utility.showToast(NoteDetailsActivity.this,"Failed while deleting note");
+                } else {
+                    Utility.showToast(NoteDetailsActivity.this, "Failed while deleting note");
                 }
             }
         });
     }
-
-
 }
